b211405b93f0b668ede1c676dbe78494198b80ce
Slokyy <slobodancvetkovic995@gmail.com>
2022-04-20 23:25:00 +0200

    Commiting finished CRUD functionality, TODO ajax tasks

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,108 @@
+<?php
+
+  namespace App\Controller;
+
+  use App\Entity\Product;
+  use App\Repository\ProductRepository;
+  use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+  use Symfony\Component\HttpFoundation\Request;
+  use Symfony\Component\HttpFoundation\Response;
+  use Symfony\Component\Routing\Annotation\Route;
+
+  #[Route('/product', name: 'product-')]
+  class ProductController extends AbstractController
+  {
+    #[Route('/', name: 'all')]
+    public function all(ProductRepository $productRepo): Response
+    {
+
+      $products = $productRepo->findAll();
+
+      return $this->render("base.html.twig", [
+        "products" => $products
+      ]);
+    }
+
+    #[Route('/show/{slug}', name: 'show', methods: 'GET')]
+    public function show($slug, ProductRepository $productRepository): Response
+    {
+      $product = $productRepository->findOneBy(['slug' => $slug]);
+      return $this->render('product/show.html.twig', ['product' => $product]);
+    }
+
+    /**
+     * @throws \Doctrine\ORM\OptimisticLockException
+     * @throws \Doctrine\ORM\ORMException
+     */
+    #[Route('/create', name: 'form-submit', methods: ['POST', 'GET'])]
+    public function createProduct(Request $request, ProductRepository $prRepo): Response
+    {
+      if($request->getMethod() == "POST" && $request->request->has('createProductSubmit'))
+      {
+        $product = new Product();
+        $newProductData = $request->request->all();
+        $product->setName(trim($newProductData['productName']));
+        $product->setDescription(trim($newProductData['productDescription']));
+        $product->setPrice(trim($newProductData['productPrice']));
+        $date = new \DateTime();
+        $date->setTime(0,0);
+        $product->setDateAdded($date);
+        $slug = strtolower(str_replace(" ", "-", $newProductData['productName']));
+        $product->setSlug($slug);
+        $prRepo->add($product);
+        return $this->redirect('/product');
+      }
+      return $this->render("product/create.html.twig");
+
+
+    }
+
+    /**
+     * @throws \Doctrine\ORM\OptimisticLockException
+     * @throws \Doctrine\ORM\ORMException
+     */
+    #[Route('/edit/{slug}', name: 'edit', methods: ['GET', 'POST'])]
+    public function editProduct($slug, Request $request, Product $product, ProductRepository $prRepo): Response
+    {
+     if($request->headers->get('referer') == null)
+      {
+        return $this->redirect('/product');
+      }
+      if($request->getMethod() == "POST" && $request->request->has('editProductSubmit'))
+      {
+        $editProductData = $request->request->all();
+        $product->setName(trim($editProductData['editProductName']));
+        $product->setDescription(trim($editProductData['editProductDescription']));
+        $product->setPrice(trim($editProductData['editProductPrice']));
+        $date = new \DateTime();
+        $date->setTime(0,0);
+        $product->setDateAdded($date);
+        $slug = strtolower(str_replace(" ", "-", trim($editProductData['editProductName']."-".$product->getId())));
+        $product->setSlug($slug);
+        $prRepo->add($product);
+        return $this->redirect("$slug");
+      }
+      $editUserOldData = [
+        "name" => $product->getName(),
+        "price" => $product->getPrice(),
+        "description" => $product->getDescription(),
+        "slug" => $product->getSlug()
+      ];
+      return $this->render('product/edit.html.twig', ['editUserData' => $editUserOldData]);
+    }
+
+    /**
+     * @throws \Doctrine\ORM\OptimisticLockException
+     * @throws \Doctrine\ORM\ORMException
+     */
+    #[Route('/delete/{id}', name: "delete")]
+    public function delete($id, ProductRepository $prRepo): Response
+    {
+      $product = $prRepo->find($id);
+      if($product == null) {
+        throw $this->createNotFoundException("No product with ID: $id");
+      }
+      $prRepo->remove($product);
+      return $this->redirect('/product');
+    }
+  }
---
+++ src/Entity/Product.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+#[ORM\Table('proizvodi')]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private int $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private ?string $name;
+
+    #[ORM\Column(type: 'decimal', precision: 10, scale: 2)]
+    private ?float $price;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private ?string $slug;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private ?string $description;
+
+    #[ORM\Column(type: 'date')]
+    private ?\DateTimeInterface $date_added;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPrice(): ?string
+    {
+        return $this->price;
+    }
+
+    public function setPrice(string $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getDateAdded(): ?\DateTimeInterface
+    {
+        return $this->date_added;
+    }
+
+    public function setDateAdded(\DateTimeInterface $date_added): self
+    {
+        $this->date_added = $date_added;
+
+        return $this;
+    }
+}
